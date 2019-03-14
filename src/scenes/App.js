0f707918697c19a41f044ea7edfbef480600a663
Jonas Hübotter <jonas.huebotter@gmail.com>
2019-03-14 17:36:28 +0100

    [#1] Microsoft Authentication (#2)

--- src/scenes/App.js
+++ src/scenes/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
+import * as MsGraph from '@microsoft/microsoft-graph-client'
 import logo from './logo.svg'
 import './App.scss'
+
+import AuthMS from './../services/auth/AuthMS'
 
 import ForYou from './App/ForYou'
 import Movies from './App/Movies'
@@ -10,16 +13,63 @@ import Find from './App/Find'
 import Settings from './App/Settings'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.userAgentApplication = new AuthMS().perform()
+    this.state = {
+      user: this.userAgentApplication.getUser(),
+      token: '',
+      client: null
+    }
+  }
+
   render() {
-    return (
-      <div className="App">
-        <Route path="/" exact component={ForYou} />
-        <Route path="/movies" component={Movies} />
-        <Route path="/shows" component={Shows} />
-        <Route path="/find" component={Find} />
-        <Route path="/settings" component={Settings} />
-      </div>
-    )
+    if (this.state.user) {
+      return (
+        <div className="App">
+          <Route path="/" exact component={ForYou} />
+          <Route path="/movies" component={Movies} />
+          <Route path="/shows" component={Shows} />
+          <Route path="/find" component={Find} />
+          <Route path="/settings" component={Settings} />
+        </div>
+      )
+    } else {
+      return (
+        <div className="App">
+          <button onClick={this.logIn.bind(this)}>Log in</button>
+        </div>
+      )
+    }
+  }
+
+  setTokenAndClient(accessToken) {
+    let graphClient = MsGraph.Client.init({
+      authProvider: (done) => {
+        done(null, accessToken)
+      }
+    })
+    console.log(`MS Graph token: ${accessToken}`)
+    this.setState({
+      token: accessToken, client: graphClient
+    })
+  }
+
+  logIn() {
+    this.userAgentApplication.loginPopup(AuthMS.config.graphScopes).then((idToken) => {
+      this.userAgentApplication.acquireTokenSilent(AuthMS.config.graphScopes).then((accessToken) => {
+         this.setTokenAndClient(accessToken)
+       }, (error) => {
+         this.userAgentApplication.acquireTokenPopup(AuthMS.config.graphScopes).then((accessToken) => {
+           this.setTokenAndClient(accessToken)
+         }, (error) => {
+           console.log(error)
+         })
+       })
+    }, (error) => {
+      console.log(error)
+    })
   }
 }
 
