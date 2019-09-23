854c2fbe4ddf12c4164a02cac55be1c6ef45c708
Jonas Hübotter <me@jonhue.me>
2019-09-23 23:30:46 +0200

    [#34] Not found scene

--- src/scenes/NotFound.js
+++ src/scenes/NotFound.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
-// import './NotFound.scss'
+import { Link } from 'react-router-dom'
+import './NotFound.scss'
 
 class NotFound extends Component {
   render() {
     return (
       <div className='NotFound'>
+        <h2>You want to get lost?</h2>
+        <p>This path went astray... If you do want to get lost, we recommend <a href='https://www.imdb.com/title/tt6969502/' target='_blank'>Get Lost! ny Mirjam de With</a>.</p>
+        <p>If all you're looking for is to get back on track: you can <Link to='/app'>find home this way</Link>.</p>
       </div>
     )
   }
