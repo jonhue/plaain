7f8f2f09b2579cb463ef37dd80b5646b66dac7bd
Jonas Hübotter <me@jonhue.me>
2019-09-23 16:30:59 +0200

    [#144] Remove unused icons (#148)

--- src/components/Nucleo/icons/cloud-upload-96.jsx
+++
@@ -1,8 +0,0 @@
-<svg height={this.props.height} width={this.props.width} viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
-	<title>cloud upload 96</title>
-	<g fill="#ffffff" transform="translate(0.5, 0.5)">
-		<line fill="none" stroke="#ffffff" strokeLinecap="round" strokeWidth="3" x1="32" x2="32" y1="48" y2="26"/>
-		<polyline fill="none" points="22,36 32,26 42,36 " stroke="#ffffff" strokeLinecap="round" strokeWidth="3"/>
-		<path d="M40,52h12 c5.6-0.5,10-5.3,10-11c0-5.8-4.4-10.5-10.1-11c-1-10.1-9.5-18-19.9-18c-9,0-16.6,6-19.1,14.2C6.7,27.2,2,32.5,2,39 c0,6.8,5.3,12.5,12,13h10" fill="none" stroke="#ffffff" strokeLinecap="round" strokeWidth="3"/>
-	</g>
-</svg>
--- src/components/Nucleo/icons/e-remove.jsx
+++
@@ -1,7 +0,0 @@
-<svg height={this.props.height} width={this.props.width} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-	<title>e remove</title>
-	<g fill="#ffffff">
-		<line fill="none" stroke="#ffffff" strokeLinecap="round" strokeWidth="2" x1="19" x2="5" y1="5" y2="19"/>
-		<line fill="none" stroke="#ffffff" strokeLinecap="round" strokeWidth="2" x1="19" x2="5" y1="19" y2="5"/>
-	</g>
-</svg>
