ebec435a266e3f4b4b6b1391b26b5543bd90e512
Jonas Hübotter <me@jonhue.me>
2019-09-23 23:46:29 +0200

    fix eslint error

--- src/scenes/NotFound.js
+++ src/scenes/NotFound.js
@@ -7,8 +7,8 @@ class NotFound extends Component {
     return (
       <div className='NotFound'>
         <h2>You want to get lost?</h2>
-        <p>This path went astray... If you do want to get lost, we recommend <a href='https://www.imdb.com/title/tt6969502/' target='_blank'>Get Lost! ny Mirjam de With</a>.</p>
-        <p>If all you're looking for is to get back on track: you can <Link to='/app'>find home this way</Link>.</p>
+        <p>This path went astray... If you do want to get lost, we recommend <a href='https://www.imdb.com/title/tt6969502/' target='_blank' rel='noopener noreferrer'>Get Lost! ny Mirjam de With</a>.</p>
+        <p>If all you&apos;re looking for is to get back on track: you can <Link to='/app'>find home this way</Link>.</p>
       </div>
     )
   }
