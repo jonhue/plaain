bf2a00c1e9a8aa23d32239a66286fa48bf9b24f8
Jonas Hübotter <me@jonhue.me>
2019-09-25 19:21:21 +0200

    [#182] Integrate BaseWrapper into index.js (#184)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import { Provider } from 'react-redux'
 import store from './store'
@@ -9,7 +10,9 @@ import { PersistGate } from 'redux-persist/integration/react'
 
 import * as serviceWorker from './serviceWorker'
 
-import BaseWrapper from './BaseWrapper'
+import App from './App'
+import Welcome from './scenes/Welcome'
+import NotFound from './scenes/NotFound'
 import Loading from './scenes/Loading'
 
 import './index.scss'
@@ -17,7 +20,13 @@ import './index.scss'
 ReactDOM.render(
   <Provider store={store}>
     <PersistGate loading={<Loading caption='Loading...' />} persistor={persistStore(store)}>
-      <BaseWrapper />
+      <Router>
+        <Switch>
+          <Route path='/' exact component={Welcome} />
+          <Route path='/app' component={App} />
+          <Route component={NotFound} />
+        </Switch>
+      </Router>
     </PersistGate>
   </Provider>,
   document.getElementById('root')
