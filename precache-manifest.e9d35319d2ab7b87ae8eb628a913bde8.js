3fa120750bfd25070474766c944677eb0eb26db6
Jonas Hübotter <me@jonhue.me>
2019-10-02 15:40:00 +0200

    Updates

---
+++ precache-manifest.e9d35319d2ab7b87ae8eb628a913bde8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da63b4fec61fe33b83c7f87e8eb9f2c9",
+    "url": "/plaain/index.html"
+  },
+  {
+    "revision": "1050436c554147dd739e",
+    "url": "/plaain/static/css/2.dd570239.chunk.css"
+  },
+  {
+    "revision": "0716603a2e6165d14b38",
+    "url": "/plaain/static/css/main.c65ed734.chunk.css"
+  },
+  {
+    "revision": "1050436c554147dd739e",
+    "url": "/plaain/static/js/2.f340b0a8.chunk.js"
+  },
+  {
+    "revision": "0716603a2e6165d14b38",
+    "url": "/plaain/static/js/main.e35ea012.chunk.js"
+  },
+  {
+    "revision": "3fb3e9e445f8ad5d733a",
+    "url": "/plaain/static/js/runtime-main.4c3bd0a5.js"
+  }
+]);
