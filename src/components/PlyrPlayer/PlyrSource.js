bcef26d5585097fcb7c3d985077ade32d5de3c8a
Jonas Hübotter <me@jonhue.me>
2019-10-02 19:23:56 +0200

    Update PlyrSource.js

--- src/components/PlyrPlayer/PlyrSource.js
+++ src/components/PlyrPlayer/PlyrSource.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 
 class PlyrSource extends Component {
   render() {
-    console.log(this.props.source.information.toString())
     return (
       <source
         src={this.props.source.url}
