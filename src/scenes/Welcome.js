7fc6a1a0c5ed7951060bc709b9fd3cda6173cdd0
Jonas Hübotter <me@jonhue.me>
2019-10-02 15:15:20 +0200

    [#33] Welcome scene

--- src/scenes/Welcome.js
+++ src/scenes/Welcome.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-// import './Welcome.scss'
+import './Welcome.scss'
 
 class Welcome extends Component {
   render() {
     return (
       <div className='Welcome'>
-        <Link to='/app' className='button primary'>Launch</Link>
+        <h1>Your movies & TV shows. Anywhere.</h1>
+        <div className='Welcome__details'>
+          <h3>Plaain lets you stream and maintain your media library from anywhere. No server needed, no fee required.</h3>
+          <Link to='/app' className='button primary'>Launch</Link>
+          <a href='https://github.com/jonhue/plaain' target='_blank' rel='noopener noreferrer' className='button'>How it works</a>
+        </div>
       </div>
     )
   }
