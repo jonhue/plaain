c6d56c2bcfbe3df937ac79eddd9012f8e34355d4
jonhue <jonhue@users.noreply.github.com>
2020-03-21 07:15:06 +0000

    Automated deployment: Sat Mar 21 07:15:06 UTC 2020 626b6e059d191f30876a8f76a6e2cdf73889e640

---
+++ precache-manifest.9184ed3922c8d54b5353cf88f569e9ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1e23ec107f8801b8507ad6eec7b8839",
+    "url": "/plaain/index.html"
+  },
+  {
+    "revision": "d60bb5809c4c1ba49714",
+    "url": "/plaain/static/css/2.4c269a7c.chunk.css"
+  },
+  {
+    "revision": "31705b07f2d339711d9b",
+    "url": "/plaain/static/css/main.7b804943.chunk.css"
+  },
+  {
+    "revision": "d60bb5809c4c1ba49714",
+    "url": "/plaain/static/js/2.0fcd2897.chunk.js"
+  },
+  {
+    "revision": "d1f7aaa38da28ebdab9d449439272f71",
+    "url": "/plaain/static/js/2.0fcd2897.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31705b07f2d339711d9b",
+    "url": "/plaain/static/js/main.03e6e8d4.chunk.js"
+  },
+  {
+    "revision": "615a16502ee1616b4ac0",
+    "url": "/plaain/static/js/runtime-main.c5c8064e.js"
+  }
+]);
