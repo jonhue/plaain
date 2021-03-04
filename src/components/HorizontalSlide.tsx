16200d25fead6317026ddd605c57189bd39f872c
Jonas Hübotter <jonas.huebotter@gmail.com>
2021-03-04 13:30:22 +0100

    fixes

--- src/components/HorizontalSlide.tsx
+++ src/components/HorizontalSlide.tsx
@@ -1,12 +1,12 @@
 import './HorizontalSlide.scss'
-import React, { useEffect } from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
 import { buildCoverUrl, buildItemUrl } from '../util'
 import { Cover } from './Cover'
 import { Link } from 'react-router-dom'
 import { Movie } from '../types/items/Movie'
+import React from 'react'
 import { Season } from '../types/items/Season'
 import { Show } from '../types/items/Show'
-import Swiper from 'swiper'
 import { useTranslation } from 'react-i18next'
 
 type HorizontalSlideProps = {
@@ -17,38 +17,29 @@ type HorizontalSlideProps = {
 export const HorizontalSlide = ({ id, items }: HorizontalSlideProps) => {
   const { t } = useTranslation()
 
-  useEffect(() => {
-    new Swiper(`.HorizontalSlide#${id} .swiper-container`, {
-      spaceBetween: 15,
-      slidesPerView: 'auto',
-      slidesOffsetBefore: 20,
-      slidesOffsetAfter: 20,
-      grabCursor: true,
-    })
-  }, [id])
-
   return (
     <div className="HorizontalSlide" id={id}>
-      <div className="swiper-container">
-        <div className="swiper-wrapper">
-          {items.length > 0 ? (
-            items.map((item, index) => (
-              <div className="swiper-slide" key={index}>
-                <Link to={buildItemUrl(item)}>
-                  <Cover
-                    url={buildCoverUrl(item.posterPath)}
-                    alt={item.title}
-                  />
-                </Link>
-              </div>
-            ))
-          ) : (
-            <div className="swiper-slide">
-              <h3>{t('Nothing found.')}</h3>
-            </div>
-          )}
-        </div>
-      </div>
+      <Swiper
+        spaceBetween={15}
+        slidesPerView="auto"
+        slidesOffsetBefore={20}
+        slidesOffsetAfter={20}
+        grabCursor
+      >
+        {items.length > 0 ? (
+          items.map((item, index) => (
+            <SwiperSlide key={index}>
+              <Link to={buildItemUrl(item)}>
+                <Cover url={buildCoverUrl(item.posterPath)} alt={item.title} />
+              </Link>
+            </SwiperSlide>
+          ))
+        ) : (
+          <SwiperSlide>
+            <h3>{t('Nothing found.')}</h3>
+          </SwiperSlide>
+        )}
+      </Swiper>
     </div>
   )
 }
--- src/components/VerticalSlide.tsx
+++ src/components/VerticalSlide.tsx
@@ -1,13 +1,16 @@
 import './VerticalSlide.scss'
-import React, { useEffect } from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import SwiperCore, { EffectCoverflow } from 'swiper'
 import { buildCoverUrl, buildItemUrl } from '../util'
 import { Cover } from './Cover'
 import { Link } from 'react-router-dom'
 import { Movie } from '../types/items/Movie'
+import React from 'react'
 import { Season } from '../types/items/Season'
 import { Show } from '../types/items/Show'
-import Swiper from 'swiper'
 import { useTranslation } from 'react-i18next'
+
+SwiperCore.use([EffectCoverflow])
 
 type VerticalSlideProps = {
   id: string
@@ -18,48 +21,39 @@ type VerticalSlideProps = {
 export const VerticalSlide = ({ id, items, path }: VerticalSlideProps) => {
   const { t } = useTranslation()
 
-  useEffect(() => {
-    new Swiper(`.VerticalSlide#${id} .swiper-container`, {
-      direction: 'vertical',
-      slidesPerView: 'auto',
-      grabCursor: true,
-      effect: 'coverflow',
-      coverflowEffect: {
-        rotate: 0,
-        stretch: 425,
-        depth: 150,
-        modifier: 1,
-        slideShadows: false,
-      },
-      history: {
-        replaceState: true,
-        key: path,
-      },
-    })
-  }, [id, path])
-
   return (
     <div className="VerticalSlide" id={id}>
-      <div className="swiper-container">
-        <div className="swiper-wrapper">
-          {items.length > 0 ? (
-            items.map((item, index) => (
-              <div className="swiper-slide" data-history={item.id} key={index}>
-                <Link to={buildItemUrl(item)}>
-                  <Cover
-                    url={buildCoverUrl(item.posterPath)}
-                    alt={item.title}
-                  />
-                </Link>
-              </div>
-            ))
-          ) : (
-            <div className="swiper-slide">
-              <h3>{t('Nothing found.')}</h3>
-            </div>
-          )}
-        </div>
-      </div>
+      <Swiper
+        direction="vertical"
+        slidesPerView="auto"
+        grabCursor
+        effect="coverflow"
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 425,
+          depth: 150,
+          modifier: 1,
+          slideShadows: false,
+        }}
+        history={{
+          replaceState: true,
+          key: path,
+        }}
+      >
+        {items.length > 0 ? (
+          items.map((item, index) => (
+            <SwiperSlide data-history={item.id} key={index}>
+              <Link to={buildItemUrl(item)}>
+                <Cover url={buildCoverUrl(item.posterPath)} alt={item.title} />
+              </Link>
+            </SwiperSlide>
+          ))
+        ) : (
+          <SwiperSlide>
+            <h3>{t('Nothing found.')}</h3>
+          </SwiperSlide>
+        )}
+      </Swiper>
     </div>
   )
 }
