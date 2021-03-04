da7b1f1d29b4e8a44fc15856790a3567b24a1be9
Jonas Hübotter <jonas.huebotter@gmail.com>
2021-03-04 22:38:07 +0100

    [#603] Fix build (#604)

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import './i18n.ts'
 import './index.scss'
 import 'whatwg-fetch'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import React, { Suspense } from 'react'
 import Base from './views'
 import { Loading } from './views/Loading'
@@ -8,10 +9,8 @@ import { PersistGate } from 'redux-persist/integration/react'
 import { Provider } from 'react-redux'
 import ReactDOM from 'react-dom'
 import { persistStore } from 'redux-persist'
-import { register } from './service-worker'
+import reportWebVitals from './reportWebVitals'
 import store from './store'
-
-register()
 
 ReactDOM.render(
   <React.StrictMode>
@@ -25,3 +24,13 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 )
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register()
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
