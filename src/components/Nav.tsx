8a176171561733b50c2a75f3a40f142578a3fcd7
Jonas Hübotter <jonas.huebotter@gmail.com>
2021-03-05 16:15:28 +0100

    [#593] Move search to nav bar (#642)

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,11 +1,12 @@
 import './Nav.scss'
+import { FindIcon } from './icons/Nucleo/FindIcon'
 import { ForYouIcon } from './icons/Nucleo/ForYouIcon'
 import { MovieIcon } from './icons/Nucleo/MovieIcon'
 import React from 'react'
 import { SettingsIcon } from './icons/Nucleo/SettingsIcon'
 import { ShowIcon } from './icons/Nucleo/ShowIcon'
 import { Tab } from './Tab'
-import { useTranslation } from 'react-i18next/*'
+import { useTranslation } from 'react-i18next'
 
 type NavProps = {
   moviesDisabled?: boolean
@@ -29,6 +30,10 @@ export const Nav = ({ moviesDisabled, showsDisabled }: NavProps) => {
         <Tab disabled={showsDisabled} to="/app/shows">
           <ShowIcon />
           <p>{t('Shows')}</p>
+        </Tab>
+        <Tab disabled={moviesDisabled && showsDisabled} to="/app/find">
+          <FindIcon />
+          <p>{t('Find')}</p>
         </Tab>
         <Tab to="/app/settings">
           <SettingsIcon />
--- src/views/app/Movies.tsx
+++ src/views/app/Movies.tsx
@@ -1,8 +1,7 @@
 import './Movies.scss'
-import { Link, Redirect } from 'react-router-dom'
-import { FindIcon } from '../../components/icons/Nucleo/FindIcon'
 import { ItemGrid } from '../../components/ItemGrid'
 import React from 'react'
+import { Redirect } from 'react-router-dom'
 import { RootState } from '../../store'
 import { VerticalSlide } from '../../components/VerticalSlide'
 import { moviesSelector } from '../../store/movies/selectors'
@@ -16,11 +15,6 @@ export const Movies = () => {
 
   return movies.length > 0 ? (
     <div className="Movies">
-      <div className="Movies__find">
-        <Link to="/app/find">
-          <FindIcon />
-        </Link>
-      </div>
       <VerticalSlide items={movies} path="movies" id="movies" />
       <ItemGrid items={movies} />
     </div>
