6d8620fdc86631808ff60d5e1724b0b91756efac
Jonas Hübotter <jonas.huebotter@gmail.com>
2021-03-05 09:44:14 +0100

    [#626] Update loader (#629)

--- src/components/icons/SoundWaveIcon.tsx
+++ src/components/icons/SoundWaveIcon.tsx
@@ -1,15 +1,22 @@
+import './SoundWaveIcon.scss'
 import { Icon } from './Icon'
 import React from 'react'
 
 export const SoundWaveIcon = () => (
-  <Icon className="SoundWave" height={50} width={50}>
+  <Icon className="SoundWave" width={1704.01} height={1653.92}>
+    <defs>
+      <style>{`.cls-1,.cls-2{fill:none;stroke:#ff2fc1;stroke-miterlimit:10;}.cls-1{stroke-width:200px;}.cls-2{stroke-width:250px;}`}</style>
+    </defs>
     <title>Plaain</title>
-    <path
-      fill="none"
-      d="M2.5 26.5h4l4-6 8 20 10-32 8 26 6-8h4"
-      stroke="#ff2fc1"
-      className="unPUVPIZ_0"
-    ></path>
-    <style data-made-with="vivus-instant">{`.unPUVPIZ_0{stroke-dasharray:108 110;stroke-dashoffset:109;animation:unPUVPIZ_draw_0 750ms ease-in-out 0ms infinite,unPUVPIZ_fade 750ms linear 0ms infinite;}@keyframes unPUVPIZ_draw{100%{stroke-dashoffset:0;}}@keyframes unPUVPIZ_fade{0%{stroke-opacity:1;}100%{stroke-opacity:1;}100%{stroke-opacity:0;}}@keyframes unPUVPIZ_draw_0{0%{stroke-dashoffset: 109}66.66666666666666%{ stroke-dashoffset: 0;}100%{ stroke-dashoffset: 0;}}`}</style>
+    <g id="Layer_2" data-name="Layer 2">
+      <g id="Layer_1-2" data-name="Layer 1">
+        <path className="cls-1 USOQBrWX_0" d="M1782.69,418"></path>
+        <path
+          className="cls-2 USOQBrWX_1"
+          d="M104.1,1202.51c184.48,277.54,285.43,342.7,342.94,323.23,171.43-58,12.92-887.15,146.05-917C628,601,693.91,681.44,825.78,842.41c120.88,147.54,205.58,274.72,275.7,258.17,23.24-5.49,47.24-38,19-470.06-17.29-265-42.4-499,1.49-505.44l.5-.06a5.54,5.54,0,0,1,.84,0c32.94,2.1,216.4,317,216.4,317,48.91,83.57,136.81,186.52,307.88,272.95"
+        ></path>
+      </g>
+    </g>
+    <style data-made-with="vivus-instant">{`.USOQBrWX_0{stroke-dasharray:0 2;stroke-dashoffset:1;animation:USOQBrWX_draw_0 950ms ease-in-out 0ms infinite,USOQBrWX_fade 950ms linear 0ms infinite;}.USOQBrWX_1{stroke-dasharray:3975 3977;stroke-dashoffset:3976;animation:USOQBrWX_draw_1 950ms ease-in-out 0ms infinite,USOQBrWX_fade 950ms linear 0ms infinite;}@keyframes USOQBrWX_draw{100%{stroke-dashoffset:0;}}@keyframes USOQBrWX_fade{0%{stroke-opacity:1;}78.94736842105263%{stroke-opacity:1;}100%{stroke-opacity:0;}}@keyframes USOQBrWX_draw_0{0%{stroke-dashoffset: 1}52.63157894736842%{ stroke-dashoffset: 0;}100%{ stroke-dashoffset: 0;}}@keyframes USOQBrWX_draw_1{26.31578947368421%{stroke-dashoffset: 3976}78.94736842105263%{ stroke-dashoffset: 0;}100%{ stroke-dashoffset: 0;}}`}</style>
   </Icon>
 )
