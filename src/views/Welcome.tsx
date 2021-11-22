943eab7d9d99c39fbaa9697c2567eb69010776e8
Jonas Hübotter <jonas.huebotter@gmail.com>
2021-11-22 14:16:45 +0100

    new welcome page

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,4 +1,5 @@
 import './Welcome.scss'
+import { Backdrop } from '../components/Backdrop'
 import { Link } from 'react-router-dom'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
@@ -8,24 +9,35 @@ export const Welcome = () => {
 
   return (
     <div className="Welcome">
-      <h1>{t('Your movies & TV shows. Anywhere.')}</h1>
-      <div className="Welcome__details">
-        <h3>
-          {t(
-            'Plaain lets you stream and maintain your media library from anywhere. No server needed, no fee required.',
-          )}
-        </h3>
-        <Link to="/app" className="button primary">
-          {t('Launch')}
-        </Link>
-        <a
-          href="https://github.com/jonhue/plaain#getting-started"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="button"
-        >
-          {t('How it works')}
-        </a>
+      <div className="Welcome__backdrop">
+        <Backdrop url={process.env.PUBLIC_URL + '/welcome.png'} />
+      </div>
+      <div className="Welcome__wrapper">
+        <h1>
+          {t('Your movies & TV shows.')}
+          <br />
+          {t('Anywhere.')}
+        </h1>
+        <div className="Welcome__details">
+          <h3>
+            {t(
+              'Plaain lets you stream and maintain your media library from anywhere. No server needed, no fee required.',
+            )}
+          </h3>
+          <div className="Welcome__details__actions">
+            <Link to="/app" className="button primary">
+              {t('Launch')}
+            </Link>
+            <a
+              href="https://github.com/jonhue/plaain#getting-started"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="button"
+            >
+              {t('How it works')}
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   )
