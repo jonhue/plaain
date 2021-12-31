5c1fa2ed74db7573a74a8f82bd3c51938f38ed02
Jonas Hübotter <jonas.huebotter@gmail.com>
2021-12-31 12:37:32 +0100

    swiper use different effect on mobile (#1101)

--- src/components/VerticalSlide.tsx
+++ src/components/VerticalSlide.tsx
@@ -1,6 +1,6 @@
 import './VerticalSlide.scss'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import SwiperCore, { EffectCoverflow } from 'swiper'
+import SwiperCore, { EffectCards } from 'swiper'
 import { buildCoverUrl, buildItemUrl } from '../util'
 import { Cover } from './Cover'
 import { Link } from 'react-router-dom'
@@ -10,7 +10,7 @@ import { Season } from '../types/items/Season'
 import { Show } from '../types/items/Show'
 import { useTranslation } from 'react-i18next'
 
-SwiperCore.use([EffectCoverflow])
+SwiperCore.use([EffectCards])
 
 type VerticalSlideProps = {
   id: string
@@ -23,37 +23,35 @@ export const VerticalSlide = ({ id, items, path }: VerticalSlideProps) => {
 
   return (
     <div className="VerticalSlide" id={id}>
-      <Swiper
-        direction="vertical"
-        slidesPerView="auto"
-        grabCursor
-        effect="coverflow"
-        coverflowEffect={{
-          rotate: 0,
-          stretch: 425,
-          depth: 150,
-          modifier: 1,
-          slideShadows: false,
-        }}
-        history={{
-          replaceState: true,
-          key: path,
-        }}
-      >
-        {items.length > 0 ? (
-          items.map((item, index) => (
-            <SwiperSlide data-history={item.id} key={index}>
-              <Link to={buildItemUrl(item)}>
-                <Cover url={buildCoverUrl(item.posterPath)} alt={item.title} />
-              </Link>
+      <div className="VerticalSlide__wrapper">
+        <Swiper
+          direction="horizontal"
+          grabCursor
+          effect="cards"
+          freeMode
+          history={{
+            replaceState: true,
+            key: path,
+          }}
+        >
+          {items.length > 0 ? (
+            items.map((item, index) => (
+              <SwiperSlide data-history={item.id} key={index}>
+                <Link to={buildItemUrl(item)}>
+                  <Cover
+                    url={buildCoverUrl(item.posterPath)}
+                    alt={item.title}
+                  />
+                </Link>
+              </SwiperSlide>
+            ))
+          ) : (
+            <SwiperSlide>
+              <h3>{t('Nothing found.')}</h3>
             </SwiperSlide>
-          ))
-        ) : (
-          <SwiperSlide>
-            <h3>{t('Nothing found.')}</h3>
-          </SwiperSlide>
-        )}
-      </Swiper>
+          )}
+        </Swiper>
+      </div>
     </div>
   )
 }
